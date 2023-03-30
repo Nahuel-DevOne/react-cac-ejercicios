3a05e1dbac62bb3bee5b4c5ee401abb7b373e337
Nahuel <nahuel.profesor@gmail.com>
2023-03-30 19:53:28 -0300

    comentando código y reduciendo la sintaxis

--- Codigo/Clase9/async-await.js
+++ Codigo/Clase9/async-await.js
@@ -1,13 +1,24 @@
-let getNameAsync = async (idPost)=>{
-
+// función asíncrona
+let getNameAsync = async idPost => {
+    // uso try y catch siempre para peticiones asíncronas
     try {
+      // espero la respuesta, que es la  data que traigo con fetch
       let resPost = await fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`);
-      let post = await  resPost.json()
-
-      let resUser = await   fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+      // convierto la data en json para luego poder acceder con notación de punto
+      let post = await resPost.json()
+      // viendo por consola lo que me traje
+      console.log(post)
+      // espero la respuesta, que es otra data que traigo con fetch
+      let resUser = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+      // convierto la data en json para luego poder acceder con notación de punto
       let user = await resUser.json()
+      // viendo por consola lo que me traje
+      console.log(user) 
 
-      document.write(`${user.name} vive en ${user.address.city} escribio el post ${idPost}: ${post.title}`) 
+      // jugando ya con la data, accediendo a la información con notación de punto
+      // para la de post va a ser post. y para la de user va a ser user. y la data a la que quiera acceder
+      document.write(`${user.name} vive en ${user.address.city} escribio el post ${idPost}: ${post.title}`)
+      console.log(`${user.name} vive en ${user.address.city} escribio el post ${idPost}: ${post.title}`)
 
     } catch (error) {
         console.log(error);
--- Codigo/Clase9/axios.js
+++ Codigo/Clase9/axios.js
@@ -1,14 +1,18 @@
-let getNameAxios = async (idPost)=>{
+// usando la librería de axios
+let getNameAxios = async idPost => {
 
     try {
         let resPost = await axios (`https://jsonplaceholder.typicode.com/posts/${idPost}`)
-   /*      console.log(resPost.data); */
+        console.log(resPost.data);
         let resUser = await axios (`https://jsonplaceholder.typicode.com/users/${resPost.data.userId}`)
-document.write(`${resUser.data.name} escribio el post ${resPost.data.title} y vive en ${resUser.data.address.city}`);
+        console.log(resUser.data)
+        // jugando con la data
+        document.write(`${resUser.data.name} escribio el post ${resPost.data.title} y vive en ${resUser.data.address.city}`);
+        console.log(`${resUser.data.name} escribio el post ${resPost.data.title} y vive en ${resUser.data.address.city}`);
 
     } catch (error) {
         console.log(error);
     }
-
 }
+
 getNameAxios(8)
