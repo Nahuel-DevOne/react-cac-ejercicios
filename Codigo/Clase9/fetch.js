083b68dbfb8bc2a0afc79539adcd784591098ce3
Nahuel <nahuel.profesor@gmail.com>
2023-03-30 20:00:28 -0300

    reduciendo la sintaxis

--- Codigo/Clase9/fetch.js
+++ Codigo/Clase9/fetch.js
@@ -1,29 +1,23 @@
-const getNamefetch =  (idPost) => {
-    try {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
-    .then((res) => {
-      /*  console.log(res) */
-      return res.json();
-    })
-    .then((post) => {
-      fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
-        .then((res) => {
-          /*  console.log(res) */
-          return res.json();
-        })
-        .then((user) => {
-          console.log(
-            `${user.name} escribio el post ${idPost} y vive en ${user.address.city}`
-          );
+const getNamefetch = idPost => {
+  try {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+      .then(res => {
+        console.log(res);
+        return res.json();
+      })
+      .then(post => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+          .then(res => {
+            console.log(res);
+            return res.json();
+          })
+          .then(user => {
+            console.log(`${user.name} escribio el post ${idPost} y vive en ${user.address.city}`);
         });
-    });  
-    } catch (error) {
-        console.log(error);
-    }
+      });
+  } catch (error) {
+    console.log(error);
+  }
 };
+
 getNamefetch(48);
-
-
-
-
-
